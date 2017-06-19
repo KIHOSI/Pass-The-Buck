5305e0c960f3c15ab11b944d36462f0694273225
unknown <>
2017-06-20 01:01:22 +0800

    Add Assets

---
+++ Assets/Game/Script/CreateRoom.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+using UnityEngine.UI;
+
+namespace Com.MyProject.MyPassTheBuckGame
+{
+	
+	public class CreateRoom : Photon.PunBehaviour{
+
+
+		#region Public Variables
+
+		public string MaxPlayerChoosed; 
+		public byte MaxPlayersPerRoom;
+		public string GameRoomName;
+		public int menuIndex;
+		public List<Dropdown.OptionData> menuOptions;
+
+
+		#endregion
+
+		#region MonoBehaviour CallBacks
+
+
+	   void Start ()
+	   {
+
+       }
+
+		#endregion
+
+		#region Public Methods
+
+		//開房間(按下創建按鈕)
+		public void CreateGameRoom()
+		{
+
+			GameRoomName = GameObject.Find ("GameRoomNameIp1").GetComponent<InputField> ().text;
+			menuOptions= GameObject.Find ("PlayerNumberDd").GetComponent<Dropdown> ().options;
+
+			menuIndex =GameObject.Find ("PlayerNumberDd").GetComponent<Dropdown> ().value;
+
+			MaxPlayerChoosed = menuOptions [menuIndex].text;
+
+			Debug.Log(MaxPlayerChoosed);
+
+			if (MaxPlayerChoosed =="2")
+			{
+				MaxPlayersPerRoom = 2;
+			}
+			else if (MaxPlayerChoosed=="3")
+			{
+				MaxPlayersPerRoom = 3;
+			}
+			else if (MaxPlayerChoosed=="4")
+			{
+				MaxPlayersPerRoom = 4;
+			}
+
+			Debug.Log(MaxPlayersPerRoom);
+
+			RoomOptions options = new RoomOptions ();
+			options.MaxPlayers = MaxPlayersPerRoom;
+
+			PhotonNetwork.CreateRoom(GameRoomName,options,null);
+
+		}
+
+		//按返回鍵
+		public void Back()
+		{
+			SceneManager.LoadScene(2);
+		}
+
+		#endregion
+
+		#region Photon.PunBehaviour CallBacks
+
+		public override void OnCreatedRoom()
+		{
+			Debug.Log("你已進入遊戲室!"+"遊戲室名稱為:"+PhotonNetwork.room.Name+"、最大遊玩人數:"+MaxPlayersPerRoom);
+			SceneManager.LoadScene(5);
+		}
+
+		public override void OnPhotonCreateRoomFailed (object[] codeAndMsg)
+		{
+			Debug.Log ("創立房間失敗");
+		}
+
+		public override void OnLeftRoom()
+		{
+			SceneManager.LoadScene(0);
+		}
+
+		#endregion
+
+	}
+}
---
+++ Assets/Game/Script/Marquee.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace Com.MyProject.MyPassTheBuckGame
+{
+	public class Marquee :Photon.PunBehaviour
+   {
+	 public string message = "歡迎來到政客踢皮球";
+	 public float scrollSpeed = 50;
+	 public GameObject MarqueeTx;
+	 Text MText;
+
+	 Rect messageRect;
+
+	 void OnGUI ()
+	 {
+		MText = MarqueeTx.GetComponent<Text> ();
+		// Set up message's rect if we haven't already.
+		if (messageRect.width == 0) {
+
+			// Start message past the left side of the screen.
+				messageRect.x = -19;
+				messageRect.width = 19;
+				messageRect.height = 0;
+		}
+
+		messageRect.x += Time.deltaTime * scrollSpeed;
+
+		// If message has moved past the right side, move it back to the left.
+		if (messageRect.x > Screen.width) {
+			messageRect.x = -messageRect.width;
+		}
+
+		GUI.Label(messageRect, message);
+	 }
+   }
+}
