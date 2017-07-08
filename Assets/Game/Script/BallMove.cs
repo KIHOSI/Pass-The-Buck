b76651980f4dfd1da583c67bc2aa9d93b0a51489
KIHOSI <amie1234@gmail.com>
2017-07-08 16:54:17 +0800

    Let Ball Move Ok

--- Assets/Game/Script/BallMove.cs
+++ Assets/Game/Script/BallMove.cs
@@ -10,57 +10,46 @@ public class BallMove : MonoBehaviour {
 
     public bool ballState = false; //球的狀態，false:左 | true:右
 
-    //public enum DragMethod { Force,SmoothDamp}; //兩種拖曳方式
-    //public DragMethod method = DragMethod.SmoothDamp; 
-
     public float speedDelta = 1.0f;
-    //public float decelerate = 1.0f;
     private bool startDrag;
     private Vector3 prePos;
+    private Vector3 clickPos; //滑鼠點選的位置
 
     void Start()
     {
         ballRigidbody2D = GetComponent<Rigidbody2D>();
         ballRigidbody2D.velocity = new Vector2(speedX, speedY);
 
-        prePos = Camera.main.WorldToScreenPoint(transform.position);
-        //ballRigidbody2D.drag = decelerate; //使用這個來讓物體逐漸停止，也可以在Rigidbody中設定drag值，這樣就可以移除這行
-    
-}
+        prePos = Camera.main.WorldToScreenPoint(transform.position); //計算滑鼠座標換算成世界座標
+    }
 	
 	
 	void Update () {
-        //switch (method)
-        //{
-       /*     case DragMethod.Force:
-                if (Input.GetKeyUp(KeyCode.Mouse0))
-                {
-                    startDrag = false;
-                }
-                if (startDrag)
-                {
-                    ForceCalculate(); //如果拖曳中，就一直給物體施加滑鼠造成的力
-                }
-            break;
-*/
+       
           //  case DragMethod.SmoothDamp:
-                if (Input.GetKeyUp(KeyCode.Mouse0)) //當按下滑鼠左鍵
+                if (Input.GetKeyUp(KeyCode.Mouse0)) //當放開滑鼠左鍵
                 {
-                    if (startDrag) //如果放開滑鼠，就停止物體追隨滑鼠的效果，並讓物體朝著最後移動的向量移動
+                    if (startDrag)
                     {
-                        ballRigidbody2D.velocity = v;
                         startDrag = false;
-                        v = Vector3.zero;
-                    }
+                      
+                        //在滑鼠放開的時候，根據一開始按球的點與放球的點距離，來做速度
+                        Vector3 curPos = Input.mousePosition;
+                        Vector3 dir = curPos - clickPos;
+                        float dist = dir.magnitude;
+                        float v = dist / Time.deltaTime;
+                            
+                        ballRigidbody2D.AddForce(dir.normalized * v * Time.deltaTime * speedDelta);
                 }
-                if (startDrag)
+
+                }
+                if (startDrag) //持續拖曳
                 {
                     prePos = Input.mousePosition;
                     TowardTarget(); //如果拖曳中，就讓物體往滑鼠的座標移動
+                    //Vector3 targetPos = Camera.main.ScreenToWorldPoint(new Vector3(prePos.x, prePos.y, 10f));
+                    //transform.position = targetPos;
                 }
-           // break;
-        //}
-
 
         if (transform.position.y > 10 || transform.position.y < -10)
         {
@@ -71,16 +60,8 @@ public class BallMove : MonoBehaviour {
 
     private void OnMouseDown() //當滑鼠點下物件的時候重設一些數值
     {
-        //switch (method)
-        //{
-        //    case DragMethod.Force:
-        //        prePos = Input.mousePosition;
-         //   break;
-
-         //   case DragMethod.SmoothDamp:
-                prePos = Camera.main.WorldToScreenPoint(transform.position);
-        //    break;
-        //}
+        prePos = Camera.main.WorldToScreenPoint(transform.position); //轉成
+        clickPos = Input.mousePosition;
         startDrag = true;
     }
 
@@ -156,25 +137,12 @@ public class BallMove : MonoBehaviour {
         }
     }
 
-    /*//施力的方式移動
-    void ForceCalculate()
-    {
-        Vector3 curPos = Input.mousePosition;
-        Vector3 dir = curPos - prePos;
-        float dist = dir.magnitude;
-        float v = dist / Time.deltaTime;
-
-        ballRigidbody2D.AddForce(dir.normalized * v * Time.deltaTime * speedDelta);
-        prePos = curPos;
-    }*/
-
     //朝著滑鼠的方式移動
     public Vector3 v;
     public float maxSpeed = 5.0f;
     void TowardTarget()
-    {
+    { //讓物體朝著最後移動的向量移動
         Vector3 targetPos = Camera.main.ScreenToWorldPoint(new Vector3(prePos.x, prePos.y, 10f)); //Assume your camera's z is -10 and cube's z is 0
         transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref v, speedDelta, maxSpeed);
     }
-
 }
