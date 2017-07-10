8ea3a8d5081109e58ae15a2e08a06ab2b796c84a
KIHOSI <amie1234@gmail.com>
2017-07-10 17:41:05 +0800

    Adjust ball move

--- Assets/Game/Script/BallMove.cs
+++ Assets/Game/Script/BallMove.cs
@@ -1,71 +1,43 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class BallMove : MonoBehaviour {
-
+public class BallMove : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
     Rigidbody2D ballRigidbody2D;
-
     public float speedX;    //球的水平速度
     public float speedY;    //球的垂直速度
-
     public bool ballState = false; //球的狀態，false:左 | true:右
 
-    public float speedDelta = 1.0f;
-    private bool startDrag;
-    private Vector3 prePos;
-    private Vector3 clickPos; //滑鼠點選的位置
+    private Vector3 prePos; //滑鼠點選位置
+    private Vector3 clickPos; //滑鼠最初點選的位置
+    private float speedDelta = 1.0f;
+
+
+    //觸控拖曳
+    public static GameObject DraggedInstance;
+
+    Vector3 _startPosition;
+    Vector3 _offsetToMouse;
+    float _zDistanceToCamera;
+
+    #region Interface Implementations
 
     void Start()
     {
         ballRigidbody2D = GetComponent<Rigidbody2D>();
         ballRigidbody2D.velocity = new Vector2(speedX, speedY);
 
-        prePos = Camera.main.WorldToScreenPoint(transform.position); //計算滑鼠座標換算成世界座標
     }
-	
-	
-	void Update () {
-       
-          //  case DragMethod.SmoothDamp:
-                if (Input.GetKeyUp(KeyCode.Mouse0)) //當放開滑鼠左鍵
-                {
-                    if (startDrag)
-                    {
-                        startDrag = false;
-                      
-                        //在滑鼠放開的時候，根據一開始按球的點與放球的點距離，來做速度
-                        Vector3 curPos = Input.mousePosition;
-                        Vector3 dir = curPos - clickPos;
-                        float dist = dir.magnitude;
-                        float v = dist / Time.deltaTime;
-                            
-                        ballRigidbody2D.AddForce(dir.normalized * v * Time.deltaTime * speedDelta);
-                }
 
-                }
-                if (startDrag) //持續拖曳
-                {
-                    prePos = Input.mousePosition;
-                    TowardTarget(); //如果拖曳中，就讓物體往滑鼠的座標移動
-                    //Vector3 targetPos = Camera.main.ScreenToWorldPoint(new Vector3(prePos.x, prePos.y, 10f));
-                    //transform.position = targetPos;
-                }
-
+    void Update()
+    {
         if (transform.position.y > 10 || transform.position.y < -10)
         {
             //如果物件的Y值大於10或小於10就將物件刪除
             Destroy(gameObject);
         }
     }
-
-    private void OnMouseDown() //當滑鼠點下物件的時候重設一些數值
-    {
-        prePos = Camera.main.WorldToScreenPoint(transform.position); //轉成
-        clickPos = Input.mousePosition;
-        startDrag = true;
-    }
-
-
     void OnCollisionEnter2D(Collision2D collision) //發生碰撞時
     {
         if (gameObject.CompareTag("左邊的球"))
@@ -76,19 +48,7 @@ public class BallMove : MonoBehaviour {
         {
             ballState = true;
         }
-        lockSpeed();
-    }
-
-    void OnTriggerEnter2D(Collider2D collision)
-    {
-        /*if (collision.gameObject.CompareTag("洞口"))
-        {
-            NowState.reduceMoney(); //減錢
-            int money = NowState.getMoney(); //得到現在錢的狀態
-            //money -= 10; //減錢
-            print("現在的錢:" + money);
-            moneyText.text = "金錢 x" + money;
-        }*/
+        //lockSpeed();
     }
 
     void lockSpeed() //保持速度
@@ -123,7 +83,7 @@ public class BallMove : MonoBehaviour {
             }
         }
     }
-    
+
     float ResetSpeedY() //保持垂直速度
     {
         float currentSpeedY = ballRigidbody2D.velocity.y; //現在球的實際水平速度
@@ -137,9 +97,44 @@ public class BallMove : MonoBehaviour {
         }
     }
 
+
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        clickPos = Input.mousePosition;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (Input.touchCount > 1)
+            return;
+
+        prePos = Input.mousePosition; //現在滑鼠位置
+        TowardTarget();
+
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        DraggedInstance = null;
+        _offsetToMouse = Vector3.zero;
+
+        //在滑鼠放開的時候，根據一開始按球的點與放球的點距離，來做速度
+        Vector3 curPos = Input.mousePosition;
+        Vector3 dir = curPos - clickPos;
+        float dist = dir.magnitude;
+        float v = dist / Time.deltaTime;
+
+        ballRigidbody2D.AddForce(dir.normalized * v * Time.deltaTime * speedDelta);
+
+    }
+
+    #endregion
+
+
     //朝著滑鼠的方式移動
-    public Vector3 v;
-    public float maxSpeed = 5.0f;
+    Vector3 v;
+    float maxSpeed = 5.0f;
     void TowardTarget()
     { //讓物體朝著最後移動的向量移動
         Vector3 targetPos = Camera.main.ScreenToWorldPoint(new Vector3(prePos.x, prePos.y, 10f)); //Assume your camera's z is -10 and cube's z is 0
