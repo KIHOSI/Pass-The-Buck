5318ba493a2f9f0a4f891f0607dfb79d91c9b3de
KIHOSI <amie1234@gmail.com>
2017-07-15 13:09:47 +0800

    Change ball move

--- Assets/Game/Script/BallMove.cs
+++ Assets/Game/Script/BallMove.cs
@@ -2,26 +2,17 @@
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class BallMove : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+public class BallMove : MonoBehaviour
 {
     Rigidbody2D ballRigidbody2D;
     public float speedX;    //球的水平速度
     public float speedY;    //球的垂直速度
-    public bool ballState = false; //球的狀態，false:左 | true:右
+    //public bool ballState = false; //球的狀態，false:左 | true:右
 
     private Vector3 prePos; //滑鼠點選位置
     private Vector3 clickPos; //滑鼠最初點選的位置
     private float speedDelta = 1.0f;
-
-
-    //觸控拖曳
-    public static GameObject DraggedInstance;
-
-    Vector3 _startPosition;
-    Vector3 _offsetToMouse;
-    float _zDistanceToCamera;
-
-    #region Interface Implementations
+    bool startDrag; //判斷是不是點到這顆球
 
     void Start()
     {
@@ -32,112 +23,12 @@ public class BallMove : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDrag
 
     void Update()
     {
-        if (transform.position.y > 10 || transform.position.y < -10)
+
+        if (transform.position.y > 5 || transform.position.y < -5)
         {
-            //如果物件的Y值大於10或小於10就將物件刪除
+            //如果物件的Y值大於5或小於5就將物件刪除(差不多離開手機範圍)
             Destroy(gameObject);
         }
-    }
-    void OnCollisionEnter2D(Collision2D collision) //發生碰撞時
-    {
-        if (gameObject.CompareTag("左邊的球"))
-        {
-            ballState = false;
-        }
-        else if (gameObject.CompareTag("右邊的球"))
-        {
-            ballState = true;
-        }
-        //lockSpeed();
-    }
+    }  
 
-    void lockSpeed() //保持速度
-    {
-        Vector2 lockSpeed = new Vector2(ResetSpeedX(), ResetSpeedY());
-        ballRigidbody2D.velocity = lockSpeed;
-    }
-
-    float ResetSpeedX() //保持水平速度
-    {
-        float currentSpeedX = ballRigidbody2D.velocity.x; //現在球的實際水平速度
-        if (ballState == true) //右邊的球
-        {
-            if (currentSpeedX < 0) //左邊
-            {
-                return speedX;
-            }
-            else //右邊
-            {
-                return -speedX;
-            }
-        }
-        else  //左邊的球
-        {
-            if (currentSpeedX < 0) //左邊
-            {
-                return -speedX;
-            }
-            else //右邊
-            {
-                return speedX;
-            }
-        }
-    }
-
-    float ResetSpeedY() //保持垂直速度
-    {
-        float currentSpeedY = ballRigidbody2D.velocity.y; //現在球的實際水平速度
-        if (currentSpeedY < 0) //下面
-        {
-            return -speedY;
-        }
-        else //上面
-        {
-            return speedY;
-        }
-    }
-
-
-
-    public void OnBeginDrag(PointerEventData eventData)
-    {
-        clickPos = Input.mousePosition;
-    }
-
-    public void OnDrag(PointerEventData eventData)
-    {
-        if (Input.touchCount > 1)
-            return;
-
-        prePos = Input.mousePosition; //現在滑鼠位置
-        TowardTarget();
-
-    }
-
-    public void OnEndDrag(PointerEventData eventData)
-    {
-        DraggedInstance = null;
-        _offsetToMouse = Vector3.zero;
-
-        //在滑鼠放開的時候，根據一開始按球的點與放球的點距離，來做速度
-        Vector3 curPos = Input.mousePosition;
-        Vector3 dir = curPos - clickPos;
-        float dist = dir.magnitude;
-        float v = dist / Time.deltaTime;
-
-        ballRigidbody2D.AddForce(dir.normalized * v * Time.deltaTime * speedDelta);
-
-    }
-
-    #endregion
-
-
-    //朝著滑鼠的方式移動
-    Vector3 v;
-    float maxSpeed = 5.0f;
-    void TowardTarget()
-    { //讓物體朝著最後移動的向量移動
-        Vector3 targetPos = Camera.main.ScreenToWorldPoint(new Vector3(prePos.x, prePos.y, 10f)); //Assume your camera's z is -10 and cube's z is 0
-        transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref v, speedDelta, maxSpeed);
-    }
 }
---
+++ Assets/Game/Script/Raycaster.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycaster : MonoBehaviour {
+    Rigidbody2D ballRigidbody2D; //存被打到的物件
+    GameObject gb;
+
+   // private Vector3 prePos; //滑鼠點選位置
+    private Vector3 clickPos; //滑鼠最初點選的位置
+    private float speedDelta = 1.0f;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, 10, 1); //偵測
+
+        if (Input.GetTouch(0).phase == TouchPhase.Began) // Began: 觸控點開始移動時
+        {
+            if(hit.collider.name == "Edge1" || hit.collider.name == "Edge2") //不能移動背景物
+            {
+                return;
+            }
+
+            gb = hit.collider.gameObject; //得到選擇的物件
+            ballRigidbody2D = hit.transform.GetComponent<Rigidbody2D>();
+            ballRigidbody2D.velocity = Vector2.zero; //把該物件的速度設為0
+            clickPos = Input.GetTouch(0).position; //是Vector2 不知道有沒有影響
+        }
+       /* if (Input.GetTouch(0).phase == TouchPhase.Moved || Input.GetTouch(0).phase == TouchPhase.Stationary) //Moved: 觸控點移動中
+        {
+            prePos = Input.GetTouch(0).position;
+            //TowardTarget();
+
+        }*/
+        if (Input.GetTouch(0).phase == TouchPhase.Ended) // Ended: 手指離開螢幕
+        {
+            //在滑鼠放開的時候，根據一開始按球的點與放球的點距離，來做速度
+            Vector3 curPos = Input.mousePosition;
+            Vector3 dir = curPos - clickPos;
+            float dist = dir.magnitude;
+            float v = dist / Time.deltaTime;
+
+            ballRigidbody2D.AddForce(dir.normalized * v * Time.deltaTime * speedDelta);
+
+            gb = null; //初始化
+            ballRigidbody2D = null;
+        }
+    }
+
+    /*//朝著滑鼠的方式移動
+    Vector3 v;
+    float maxSpeed = 5.0f;
+    void TowardTarget()
+    {
+        Vector3 targetPos = Camera.main.ScreenToWorldPoint(new Vector3(prePos.x, prePos.y, 10f)); //Assume your camera's z is -10 and cube's z is 0
+        gb.transform.position = Vector3.SmoothDamp(gb.transform.position, targetPos, ref v, speedDelta, maxSpeed);
+    }*/
+}
